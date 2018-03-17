7c1b2795fc4a82ec06f8d2adbef32c7eb4cddb41
Instrutor <felpmel@gmail.com>
2018-03-17 16:55:28 -0300

    Capítulo metodos

--- server/main.js
+++ server/main.js
@@ -1,5 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 
+Meteor.publish('thePlayers', function(){
+    var currentUserId = this.userId;
+    return PlayersList.find({createdBy:currentUserId});
+});
+
+Meteor.methods({
+    'insertPlayerData':function(playerNameVar){
+        var currentUserId = Meteor.userId();
+        PlayersList.insert({
+            name:playerNameVar,
+            score:0,
+            createBy:currentUserId});
+            
+            console.log("passou por aqui");
+    }
+    
+});
+
+
 Meteor.startup(() => {
     
 });
